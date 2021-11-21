952642e168c8cc7ca6a408354c77ef25e00a8620
Hardik Jogani <joganihardik57@gmail.com>
2021-11-20 23:18:31 -0800

    Dark Theme

---
+++ assets/js/theme.js
@@ -0,0 +1,26 @@
+const toggleSwitch = document.getElementById('cb5');
+
+function switchTheme(e) {
+    if (e.target.checked) {
+        document.documentElement.setAttribute('data-theme', 'dark');
+        localStorage.setItem('theme', 'dark');
+    }
+    else {
+        document.documentElement.setAttribute('data-theme', 'light');
+        localStorage.setItem('theme', 'light');
+    }  
+}
+
+if(toggleSwitch != null) {
+    toggleSwitch.addEventListener('change', switchTheme, false);
+}
+
+const currentTheme = localStorage.getItem('theme') ? localStorage.getItem('theme') : null;
+
+if (currentTheme) {
+    document.documentElement.setAttribute('data-theme', currentTheme);
+
+    if (currentTheme === 'dark') {
+        toggleSwitch.checked = true;
+    }
+}
